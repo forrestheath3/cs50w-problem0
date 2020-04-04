4011b92d00d7ec19ab1a4e40f3b35d45a35ae598
Forrest Heath 3 <forrest@pareto-design.com>
2020-04-04 10:25:32 -0500

    added all the source files

---
+++ code.js
@@ -0,0 +1,3 @@
+document.querySelector("#musk-button").onclick = function() {
+                document.querySelector("bodypage1").style.backgroundImage = "https://media.giphy.com/media/5eFRL3jPLgbNj6iwPx/giphy.gif";
+            }
